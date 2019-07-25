ffc9f6616f69ced0d23c88b6874745498bd5e59b
rsaemann <saemann@hydromech.uni-hannover.de>
2019-07-25 10:44:20 +0200

    * RadolanReader
    - Datastructure moved to other class
    + Main method and display Frame
    + comments

--- RadolanReader.java
+++ RadolanReader.java
@@ -25,20 +25,15 @@ package rain;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.awt.image.Raster;
-import java.awt.image.WritableRaster;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -62,21 +57,23 @@ import javax.swing.TransferHandler;
  */
 public class RadolanReader {
 
-    String product;
-    int[][] values;
-    private int x;
-    private int y;
-    private int leadTime;
-    private GregorianCalendar cal;
-
-    public void read(File gzipFile) throws IOException {
+    /**
+     * If file is zipped, it is decrypted to temp-directory first and
+     * readRawData afterwards.
+     *
+     * @param gzipFile
+     * @return
+     * @throws IOException
+     */
+    public RadolanData readFile(File gzipFile) throws IOException {
         byte[] buffer = new byte[1024];
         //Create temporary file
         File decompressedFile;
-
+        boolean deletatEnd = false;
         if (gzipFile.getName().endsWith(".gz")) {
+            //FIle needs to be decrypted first.
             decompressedFile = File.createTempFile(gzipFile.getName(), "");
-
+            decompressedFile.deleteOnExit();
             try {
 
                 FileInputStream fileIn = new FileInputStream(gzipFile);
@@ -91,32 +88,36 @@ public class RadolanReader {
 
                     fileOutputStream.write(buffer, 0, bytes_read);
                 }
-
+                deletatEnd = true;
                 gZIPInputStream.close();
                 fileOutputStream.close();
-
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         } else {
             decompressedFile = gzipFile;
         }
-        FileInputStream fis = new FileInputStream(decompressedFile);
-        InputStreamReader isr = new InputStreamReader(fis);
-
-        read(isr, fis);
-        isr.close();
-        fis.close();
+        RadolanData r = readRawData(decompressedFile);
+        if (deletatEnd) {
+            try {
+                decompressedFile.delete();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return r;
 
     }
 
-    public void read(InputStreamReader stream, InputStream is) throws IOException {
-        BufferedReader br = new BufferedReader(stream);
-        br.mark(100);
+    public RadolanData readRawData(File f) throws IOException {
+
+        FileInputStream fis = new FileInputStream(f);
+        InputStreamReader sr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(sr);
+
         String line = br.readLine();
-//        System.out.println(line);
         //Read header
-        product = line.substring(0, 2);
+        String product = line.substring(0, 2);
         int day = Integer.parseInt(line.substring(2, 4));
         int hour = Integer.parseInt(line.substring(4, 6));
         int minute = Integer.parseInt(line.substring(6, 8));
@@ -127,10 +128,10 @@ public class RadolanReader {
         int markeBY = line.indexOf("BY");
         if (!line.substring(17, 19).equals("BY")) {
             System.err.println("Exception: BY not at location 17");
-            return;
+            return null;
         }
         //Create UTC time object (DWD data is in UTC timezone format)
-        cal = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
         cal.set(GregorianCalendar.YEAR, year + 2000);
         cal.set(GregorianCalendar.MONTH, month - 1);
         cal.set(GregorianCalendar.DAY_OF_MONTH, day);
@@ -151,9 +152,7 @@ public class RadolanReader {
 
         int lengthText = Integer.parseInt(line.substring(markeMS + 2, line.indexOf("<", markeMS)).trim());
         int markeST = line.indexOf("ST", markeMS + lengthText);
-        if (markeST < 0) {
-//            System.err.println("no ST marking.");
-        }
+
         int markeETX = line.indexOf('\u0003');
         if (markeETX < 0) {
             markeETX = line.indexOf("\\x03");
@@ -171,121 +170,103 @@ public class RadolanReader {
             grid = line.substring(markeGP + 2, markeVV);
         }
         int posX = grid.indexOf("x");
-        x = Integer.parseInt(grid.substring(0, posX).trim());
-        y = Integer.parseInt(grid.substring(posX + 1).trim());
+        int x = Integer.parseInt(grid.substring(0, posX).trim());
+        int y = Integer.parseInt(grid.substring(posX + 1).trim());
 
         //Forecast time (lead time) [Minutes]
+        int leadTime;
         if (markeVV < 0) {
             leadTime = -1;
         } else {
             leadTime = Integer.parseInt(line.substring(markeVV + 2, markeMF).trim());
         }
 
-//        System.out.println(line);
-//        System.out.println(cal);
-//        System.out.println(cal.getTime());
         System.out.println("Grid:" + x + "x " + y);
         System.out.println("Leadtime:" + leadTime + "min");
         System.out.println("Information: " + contentLength + " byte");
-//        System.out.println("start data at pointer" + markeETX);
 
-        //Read values
+        br.close();
+        sr.close();
+        fis.close();
+
+        fis = new FileInputStream(f);
+
+        //Read value grid
         byte[] buffer = new byte[contentLength];
-
-        is.skip(markeETX + 2);
-        is.read(buffer);
-
+        //Jump to content start point
+        fis.skip(markeETX + 2);
+        fis.read(buffer);
         ByteBuffer bb = ByteBuffer.wrap(buffer);
         bb.order(ByteOrder.BIG_ENDIAN);
-        StringBuilder str = new StringBuilder(2000);
-        this.values = new int[y][x];
-        int count = 0;
-        int lastX=-1,lastY=-1;
-        for (int i = 0; i < x; i++) {
-            str = new StringBuilder(2000);
-
-            for (int j = 0; j < y; j++) {
+        int[][] values = new int[y][x];
+        //Values are stored horizontal line-wise
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
                 //decode 2-byte content
-                int pos = bb.position();
-//                int s = bb.get();//steuerbits
-//                int v = bb.get();
-//                int addition = (s & 0x0F) << 8;
                 int complete = bb.getShort();
 
+                //Value only in bit 1-12
                 int v = complete & 0x0FFF;
 
-//                ByteBuffer tbb=ByteBuffer.wrap(bbuffer);
-//                tbb.order(ByteOrder.BIG_ENDIAN);
-//                int cc=tbb.getShort();
-//                v=cc&0x0FFF;
                 if (v != 0) {
-//                    System.out.println(byteString(bbuffer)+" // "+byteString(tbb.array())+" wert: "+v+"   ,  "+(cc&0x0FFF));//+"  addiditon:"+addition+" = "+(v+addition));
-//                    v+=addition;
-//                    if((complete&0xF000)!=0){
-//                        System.out.println("Sonderfall "+byteString(bbuffer));
-//                    }
-                    byte[] bbuffer = new byte[2];
-                    bb.position(pos);
-                    bb.get(bbuffer);
-//                if(addition>0)System.out.println(addition+" wert: "+v);
                     if ((complete & 0x8000) != 0) {
-                        System.out.println(byteString(bbuffer));
-                        System.out.println(complete + " clutter in " + count);
+                        //Clutter Mark
                     }
                     if ((complete & 0x2000) != 0) {
-//                        System.out.println(byteString(bbuffer));
-//                        System.out.println("Fehlkennung");
+                        //Error mark
                         v = Integer.MIN_VALUE;
-                        lastX=i;
-                        lastY=j;
                     }
                     if ((complete & 0x4000) != 0) {
-                        System.out.println(byteString(bbuffer));
-                        System.out.println("Negatives Vorzeichen");
+                        //Negative value
                         v = -v;
                     }
                 }
-                if (v > 0) {
-                    str.append(",");
-                } else if(v==Integer.MIN_VALUE){
-                    str.append("W");
-                }else{
-                    str.append(" ");
-                }
-                this.values[i][j] = v;
-                count++;
+                values[i][j] = v;
             }
-            System.out.println(str.toString());
-
-        }
-        System.out.println("Counted " + count);
-        
-        //reorder values
-        System.out.println("lastMinvalue: "+lastX+", "+lastY);
-        if((lastX>0&&lastX<x-1)||(lastY>0&&lastY<y-1)){
-            System.out.println("Reorder values");
-            int[][] newValues=new int[values.length][values[0].length];
-            for (int i = 0; i < newValues.length; i++) {
-                for (int j = 0; j < newValues[i].length; j++) {
-                    newValues[i][j]=values[(i+lastX)%values.length][(j+lastY)%values[0].length];                    
-                }
-            }
-            this.values=newValues;
-        }
-        
-        
-        if (true) {
-            return;
         }
 
-        int i = 0;
-        while (br.ready()) {
-            line = br.readLine();
-            System.out.println(i + ")" + line);
-            i++;
+        double lowerleftLat, lowerleftLon, upperleftLat, upperleftLon, lowerRightLat, lowerRightLon, upperRightLat, upperRightLon;
+        if (x == 900 && y == 900) {
+            lowerleftLat = 46.9526;
+            lowerleftLon = 3.5889;
+            upperleftLat = 54.5877;
+            upperleftLon = 2.0715;
+
+            lowerRightLat = 47.0705;
+            lowerRightLon = 14.6209;
+            upperRightLat = 54.7405;
+            upperRightLon = 15.7208;
+        } else if (x == 900 && y == 1100) {
+            lowerleftLat = 46.1929;
+            lowerleftLon = 4.6759;
+            upperleftLat = 55.5482;
+            upperleftLon = 3.0889;
+
+            lowerRightLat = 46.1827;
+            lowerRightLon = 15.4801;
+            upperRightLat = 55.5342;
+            upperRightLon = 17.1128;
+        } else {
+            System.err.println("Do not know corners for x=" + x + " , y=" + y + " grid.");
+            lowerleftLat = 46.9526;
+            lowerleftLon = 3.5889;
+            upperleftLat = 54.5877;
+            upperleftLon = 2.0715;
+
+            lowerRightLat = 47.0705;
+            lowerRightLon = 14.6209;
+            upperRightLat = 54.7405;
+            upperRightLon = 15.7208;
         }
+        return new RadolanData(product, values, x, y, leadTime, cal, lowerleftLat, lowerleftLon, upperleftLat, upperleftLon, lowerRightLat, lowerRightLon, upperRightLat, upperRightLon);
     }
 
+    /**
+     * Helping method to display a bytebuffers content.
+     *
+     * @param bytes
+     * @return
+     */
     public static String byteString(byte[] bytes) {
         StringBuilder str = new StringBuilder(bytes.length * 20);
         for (int i = 0; i < bytes.length; i++) {
@@ -302,95 +283,48 @@ public class RadolanReader {
         return str.toString();
     }
 
-    public void read2(InputStreamReader stream) throws IOException {
-        BufferedReader br = new BufferedReader(stream);
-        br.mark(100);
-        String line = br.readLine();
-        //Read header
-        product = line.substring(0, 2);
-        int day = Integer.parseInt(line.substring(2, 4));
-        int hour = Integer.parseInt(line.substring(4, 6));
-        int minute = Integer.parseInt(line.substring(6, 8));
-        String radarID = line.substring(8, 13);
-        int month = Integer.parseInt(line.substring(13, 15));
-        int year = Integer.parseInt(line.substring(15, 17));
-
-        int markeBY = line.indexOf("BY");
-        if (!line.substring(17, 19).equals("BY")) {
-            System.err.println("Exception: BY not at location 17");
-            return;
-        }
-        //Create UTC time object (DWD data is in UTC timezone format)
-        GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        cal.set(GregorianCalendar.YEAR, year + 2000);
-        cal.set(GregorianCalendar.MONTH, month - 1);
-        cal.set(GregorianCalendar.DAY_OF_MONTH, day);
-        cal.set(GregorianCalendar.HOUR_OF_DAY, hour);
-        cal.set(GregorianCalendar.MINUTE, minute);
-        cal.set(GregorianCalendar.SECOND, 0);
-        cal.set(GregorianCalendar.MILLISECOND, 0);
-
-        int markeVS = line.indexOf("VS");
-        int contentLength = Integer.parseInt(line.substring(markeBY, markeVS));
-
-        int markeSW = line.indexOf("SW", markeBY);
-        int markeINT = line.indexOf("INT", markeSW);
-        int markeGP = line.indexOf("GP", markeINT);
-
-        System.out.println(line);
-        System.out.println(cal);
-        System.out.println(cal.getTime());
-
-        if (true) {
-            return;
-        }
-
-        int i = 0;
-        while (br.ready()) {
-            line = br.readLine();
-            System.out.println(i + ")" + line);
-            i++;
-        }
-    }
-
-    public BufferedImage createImage() {
-        if (this.values == null) {
-            throw new NullPointerException("No data values read.");
-        }
-        BufferedImage bi = new BufferedImage(this.values.length, this.values[0].length, BufferedImage.TYPE_INT_RGB);
-        WritableRaster raster = bi.getRaster();
-        for (int iy = 0; iy < values.length; iy++) {
-            int yt = y - 1 - iy;//inverse Y for top down orientation
-            for (int xt = 0; xt < values[0].length; xt++) {
-                int value = values[iy][xt];
-                if (value == Integer.MIN_VALUE) {
-                    raster.setPixel(xt, yt, new int[]{50, 50, 50});
-                } else {
-                    raster.setPixel(xt, yt, new int[]{(int) Math.min(255, Math.max(0, value > 0 ? 100 : 0) + value * 10), value > 0 ? 200 : 0, 0/*(int) Math.min(255, Math.max(0, (value*10)) )*/});
-                }
-            }
-        }
-        bi.setData(raster);
-
-        return bi;
-    }
-
     public static void main(String[] args) {
+
+        // File to load (bin/.gz/raw data)
+        File file = new File("C:\\Users\\saemann\\Desktop\\RQ1907221600_000.gz");
+
+        final JFrame frame = new JFrame(file.getAbsolutePath() + "    Robert Sämann 2019");
         try {
-            File file = new File("C:\\Users\\saemann\\Desktop\\RQ1907221600_000.gz");
-
+            //Decode data
             final RadolanReader rr = new RadolanReader();
-            rr.read(file);
+            RadolanData data = rr.readFile(file);
 
-            final JFrame frame = new JFrame(file.getAbsolutePath());
+            //Prepare frame size
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setBounds(100, 100, 1000, 1000);
+            frame.setBounds(100, 100, data.x + 200, data.y + 50);
             frame.setVisible(true);
-            final JLabel label = new JLabel("Label");
+            final JLabel label = new JLabel("Placeholder for Picture");
+
+            label.setToolTipText("Drag & Drop file here to show content.");
             frame.setLayout(new BorderLayout());
             frame.add(label, BorderLayout.CENTER);
-            label.setIcon(new ImageIcon(rr.createImage()));
-            label.setText("<html>" + file.getName() + "<br>" + rr.cal.getTime().toGMTString() + "<br>" + rr.x + " x " + rr.y + "<br>" + rr.leadTime + "min lead</html>");
+
+            //Mark some cities in the picture
+            BufferedImage bi = data.createImage();
+            Graphics g = bi.getGraphics();
+            g.setColor(Color.magenta);
+
+            double[] dataXY = data.getPositionIndicesForLatLon(52.517892, 13.385468); //Berlin
+            g.fillOval((int) (dataXY[0] - 2), (int) (dataXY[1] - 2), 6, 6);
+
+            dataXY = data.getPositionIndicesForLatLon(52.380629, 9.727707); //Hannover
+            g.drawOval((int) (dataXY[0] - 3), (int) (dataXY[1] - 3), 6, 6);
+            //crosshair to show city without overriding the conent around
+            g.drawLine(0, (int) (dataXY[1]), (int) (dataXY[0]-20), (int) (dataXY[1]));
+             g.drawLine((int) (dataXY[0]+20), (int) (dataXY[1]), data.x, (int) (dataXY[1]));
+            g.drawLine((int) (dataXY[0]), 0, (int) (dataXY[0]), (int) (dataXY[1]-20));
+            g.drawLine((int) (dataXY[0]), (int) (dataXY[1]+20), (int) (dataXY[0]), data.y);
+            
+
+            // display picture on the frame
+            label.setIcon(new ImageIcon(bi));
+            //Display information nect to the picture
+            label.setText(data.toHTMLString());
 
             label.setTransferHandler(new TransferHandler(null) {
                 @Override
@@ -419,13 +353,15 @@ public class RadolanReader {
                     List<File> data;
                     try {
                         data = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
-                        rr.read(data.get(0));
-                        BufferedImage bi = rr.createImage();
-
+                        RadolanData d = rr.readFile(data.get(0));
+                        BufferedImage bi = d.createImage();
+                        Graphics g = bi.getGraphics();
+                        g.setColor(Color.magenta);
+                        g.fillOval(530, 900 - 603, 3, 3);
                         ImageIcon ii = new ImageIcon(bi);
                         label.setIcon(ii);
-//                    label.setText("Drag & Drop file here to show content.");
-                        label.setText("<html>" + data.get(0).getName() + "<br>" + rr.cal.getTime().toGMTString() + "<br>" + rr.x + " x " + rr.y + "<br>" + rr.leadTime + "min lead</html>");
+
+                        label.setText(d.toHTMLString());
 
                         frame.setTitle(data.get(0).getAbsolutePath());
                     } catch (Exception e) {
@@ -443,8 +379,9 @@ public class RadolanReader {
                 }
             });
 
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             Logger.getLogger(RadolanReader.class.getName()).log(Level.SEVERE, null, ex);
+            frame.setTitle(ex.getLocalizedMessage());
         }
     }
 }
